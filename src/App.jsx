53871d5482e9bf3748d8d0aa47a426f8b6f8c74c
yongmin <vmflxu@gmail.com>
2023-02-25 13:46:28 +0900

    exam1

--- src/App.jsx
+++ src/App.jsx
@@ -3,64 +3,32 @@ import "App.css"
 
 function App() {
 
-  // hook : 어떠한 기능을 하는 함수
-  // useState('initial Value');
-  // ==> [state, setState] 이런 배열을 리턴해줌
-  // 이걸 구조분해할당으로
-  // {state, setState} = userState('initial Value');
-  
-  // userState, useEffect, useContext, useMemo 이런 훅들이 있음.
-  // useDispatch, use~~~ 많음.
 
-
-
-  let one = {
-    title : "title1 by Object",
-    content : "content1 by Object"
+  let [user,setUser] = useState(['','']);
+  const detectChangeID = (e) =>{
+    user[0] = e.target.value;
+    user = [...user];
+    setUser(user);
   }
-  let two = {
-    title : "title2 by Object",
-    content : "content2 by Object"
+  const detectChangePW = (e) =>{
+    user[1] = e.target.value;
+    user = [...user];
+    setUser(user);
   }
-  let three = {
-    title : "title3 by Object",
-    content : "content3 by Object"
-  }
-  const [each,setEach] = useState([one,two,three]);
-
-  // const change = () =>{
-  //   three.content = "글3은 한글로 변한다."
-  //   console.log(three);
-  // }
-  
-  const changeByState = () => {
-    three.content = "글3은 한글로 변한다."
-    setEach([one,two,three]);
-  }
-
   return (
     <div>
-      <div className='frame'>
-        <div className="bg-black">
-          <h1>{each[0].title}</h1>
-          <div className='bg-white'>
-            <h4>{each[0].content}</h4>
-          </div>
-        </div>
-        <div className="bg-black">
-          <h1>{each[1].title}</h1>
-          <div className='bg-white'>
-          <h4>{each[1].content}</h4>
-          </div>
-        </div>
-        <div className="bg-black">
-          <h1>{each[2].title}</h1>
-          <div className='bg-white'>
-          <h4>{each[2].content}</h4>
-          </div>
-        </div>
-      </div>
-      <button onClick={changeByState}>버튼을 누르면 content3가 변해야함</button>
+      아이디: <input type="text" value={user[0]} onChange={detectChangeID}></input>
+      <br />
+      비밀번호: <input type="password" value={user[1]} onChange={detectChangePW}></input>
+      <br />
+      <button onClick={() => {
+        alert(`
+        고객님이 입력하신 아이디는 ${user[0]}이며,
+        비밀번호는 ${user[1]}입니다.
+        `)
+        setUser(['','']);
+      }}
+      >로그인</button>
     </div>
   );
 }
