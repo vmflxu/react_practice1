9886791ba563a326f74b5f40f2c078dab325c2c7
yongmin <vmflxu@gmail.com>
2023-02-25 19:11:38 +0900

    exam2-1

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,20 @@ import "App.css"
 function App() {
 
 
-  let [user,setUser] = useState(['','']);
-  const detectChangeID = (e) =>{
-    user[0] = e.target.value;
-    user = [...user];
-    setUser(user);
-  }
-  const detectChangePW = (e) =>{
-    user[1] = e.target.value;
-    user = [...user];
-    setUser(user);
-  }
+
+  let [count,setCount] = useState(0);
   return (
     <div>
-      아이디: <input type="text" value={user[0]} onChange={detectChangeID}></input>
-      <br />
-      비밀번호: <input type="password" value={user[1]} onChange={detectChangePW}></input>
+      <span>{count}</span>
       <br />
       <button onClick={() => {
-        alert(`
-        고객님이 입력하신 아이디는 ${user[0]}이며,
-        비밀번호는 ${user[1]}입니다.
-        `)
-        setUser(['','']);
+        setCount(--count);
       }}
-      >로그인</button>
+      >-</button>
+      <button onClick={() => {
+        setCount(++count);
+      }}
+      >+</button>
     </div>
   );
 }
